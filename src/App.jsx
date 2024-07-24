b29ffb02d556d40733b940947706ecf21cbfdf58
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-07-24 21:05:58 +0530

    Login page and Signup page Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
+import Login from "./pages/login/Login";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Signup from "./pages/signup/Signup";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  <>
-
-  </>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
